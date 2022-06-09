0ccca6cd705fd5c86323fb63875cd18f7800c7ce
Hejdušek Jiří <Jiri.Hejdusek@tescosw.cz>
2022-06-09 15:24:46 +0200

    Union bugfix

--- goalist/union.go
+++ goalist/union.go
@@ -4,9 +4,16 @@ func (l Goalist[T]) Union(unionList Goalist[T]) Goalist[T] {
 	result := l.Distinct()
 
 	for _, item := range l {
-		if !l.Contains(item) {
+		if !result.Contains(item) {
 			result.Add(item)
 		}
 	}
+
+	for _, item := range unionList {
+		if !result.Contains(item) {
+			result.Add(item)
+		}
+	}
+
 	return result
 }
--- goalist/union_test.go
+++ goalist/union_test.go
@@ -25,10 +25,19 @@ func TestUnionOneSame(t *testing.T) {
 	assert.Len(t, united, 1)
 }
 
+func TestUnionEmptyFirst(t *testing.T) {
+	numbers1 := goalist.Goalist[int]{}
+	numbers2 := goalist.Goalist[int]{0, 0, 1, 1, 1, 2, 3, 4, 5, 6, 1024}
+
+	united := numbers1.Union(numbers2)
+	testUnited(t, united, numbers1, numbers2)
+}
+
 func testUnited[T comparable](t *testing.T, united goalist.Goalist[T], goalist1 goalist.Goalist[T], goalist2 goalist.Goalist[T]) {
 	for _, item := range united {
 		assert.Equal(t, 1, united.Count(func(x T) bool { return x == item }))
-		assert.True(t, goalist1.Contains(item))
-		assert.True(t, goalist2.Contains(item))
+
+		// item in any of two lists
+		assert.True(t, goalist1.Contains(item) || goalist2.Contains(item))
 	}
 }
