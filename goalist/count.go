990768b6ac8716ec9e37b26ac723564750abcb24
Hejdušek Jiří <Jiri.Hejdusek@tescosw.cz>
2022-06-10 11:41:50 +0200

    count as gorountine

--- goalist/count.go
+++ goalist/count.go
@@ -1,14 +1,31 @@
 package goalist
 
+import "sync"
+
 // Get count of items that meet the predicate
 func (l Goalist[T]) Count(predicate func(x T) bool) int {
 	counter := 0
 
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+
 	for _, item := range l {
-		if predicate(item) {
-			counter++
-		}
+
+		wg.Add(1)
+		go func(predicateItem T) {
+			defer wg.Done()
+
+			if predicate(predicateItem) {
+				lock.Lock()
+				defer lock.Unlock()
+
+				counter++
+			}
+
+		}(item)
 	}
+
+	wg.Wait()
 
 	return counter
 }
