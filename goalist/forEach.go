63df2702ffd1bc936ed6f1578464141a231db89e
Hejdušek Jiří <Jiri.Hejdusek@tescosw.cz>
2022-06-10 14:21:20 +0200

    foreach, waitgroup

--- goalist/forEach.go
+++ goalist/forEach.go
@@ -1,5 +1,7 @@
 package goalist
 
+import "sync"
+
 // Iterate over the goalist and call the function from parameter on each one
 func (l Goalist[T]) ForEach(function func(x T)) {
 	for _, item := range l {
@@ -9,7 +11,17 @@ func (l Goalist[T]) ForEach(function func(x T)) {
 
 // Iterate over the goalist and call the function from parameter on each one
 func (l Goalist[T]) GoForEach(function func(x T)) {
+	var wg sync.WaitGroup
+
 	for _, item := range l {
-		go function(item)
+		wg.Add(1)
+
+		go func(functionParam T) {
+			defer wg.Done()
+
+			go function(functionParam)
+		}(item)
 	}
+
+	wg.Wait()
 }
