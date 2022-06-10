10fd25e2819b65ed72687fef3af938e03eff51bd
Hejdušek Jiří <Jiri.Hejdusek@tescosw.cz>
2022-06-10 09:41:40 +0200

    goalist.All goroutine

--- goalist/all.go
+++ goalist/all.go
@@ -1,12 +1,50 @@
 package goalist
 
+import "context"
+
 // Return true if all values meet the predicate
 func (l Goalist[T]) All(predicate func(x T) bool) bool {
+	allCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	doneChan := make(chan struct{}, len(l))
+	allResultChan := make(chan bool, len(l))
+
 	for _, item := range l {
-		if !predicate(item) {
-			return false
-		}
+		go func(ctx context.Context, cancelation context.CancelFunc, predicateItem T) {
+			defer func() { doneChan <- struct{}{} }()
+
+			for {
+				select {
+				case <-ctx.Done():
+					allResultChan <- false
+					return
+				default:
+					if !predicate(predicateItem) {
+						allResultChan <- false
+						cancelation()
+					} else {
+						allResultChan <- true
+					}
+
+					return
+				}
+			}
+		}(allCtx, cancel, item)
 	}
 
-	return true
+	doneCounter := 0
+	for {
+		select {
+		case <-doneChan:
+			doneCounter++
+			if doneCounter == len(l) {
+				return true
+			}
+		case result := <-allResultChan:
+			if !result {
+				return false
+			}
+		}
+	}
 }
--- goalist/all_test.go
+++ goalist/all_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAllExists(t *testing.T) {
+func TestAllExist(t *testing.T) {
 	numbers := goalist.Goalist[int]{1, 1, 1, 1}
 	assert.True(t, numbers.All(func(x int) bool { return x == 1 }))
 }
 
-func TestAllNotExisis(t *testing.T) {
+func TestAllNotExist(t *testing.T) {
 	numbers := goalist.Goalist[int]{1, 1, 1, 1}
 	assert.False(t, numbers.All(func(x int) bool { return x == 2 }))
 }
